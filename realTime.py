937861fa4c32fae87a318b62fffe636a889792a1
Leonardo Nunes <leonardovnun@gmail.com>
2019-11-02 17:20:49 -0300

    second commit

--- realTime.py
+++ realTime.py
@@ -4,27 +4,34 @@ import matplotlib.pyplot as plt
 import time
 import threading
 
-class main:
-    def __init__(self):
-        self.preco = []
-        self.tempo = []
+tot = 0
 
-    def stonks(self):
-        r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=PETR4.SA&interval=5min&apikey=238423')
-        data = r.json()
+def stonks():
+    global tot
+    global tempo, preco
+    tempo = []
+    preco = []
+    r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=PETR4.SA&interval=5min&apikey=238423')
+    data = r.json()
 
-        timeSeries = data["Time Series (5min)"]
-        self.preco = [float(dado["1. open"]) for dado in timeSeries.values()]
+    timeSeries = data["Time Series (5min)"]
+    preco = [float(dado["4. close"]) for dado in timeSeries.values()]
+    preco.reverse()
+    
+    for i in range(len(preco)):
+        tempo.append(i)
+    tot += 1
+    
 
-        for i in range(len(self.preco)):
-            self.tempo.append(i)
+def atualizaGrafico():
+    while True:
+        stonks()
+        print(len(preco))
+        print('--------------------------------------')
+        print(len(tempo))
+        plt.plot(tempo, preco)
+        plt.savefig('images/'+'c'+str(tot)+'books_read.png')
+        # plt.show()
+        time.sleep(5)
 
-    def atualizaGrafico(self):
-        while True:
-            self.stonks()
-            plt.plot(self.tempo, self.preco)
-            plt.show()
-
-investir = main()
-investir.atualizaGrafico()
-
+atualizaGrafico()
